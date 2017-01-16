0c03da304fc9e51f4f36529033cfabff6d425521
ringkubd <ajr.jahid@gmail.com>
2017-01-16 10:44:42 +0600

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * Author: MD. ANWAR JAHID
+ * Email: ajr.jahid@gmail.com
+ * Facebook: www.fb.com/ringkubd
+ * Twitter: ringkubd
+ * Skype: ringkubd
+ */
+ //define important constant for getting db information
+define("DB_HOST", "localhost"); // Put your db host name where placed "localhost"
+define("DB_USER", "root"); // Put your db user name where placed "root"
+define("DB_PASSWORD", "password"); // Put your db password  where placed "password"
+define("DB", "angel"); // Put your db  name where placed "angel"
